091b9d086bc06f02fc9900f6a22f1a9f64ed4550
Andy <anlaics2@gmail.com>
2018-07-01 12:44:55 -0700

    working one dollar price filter

--- src/screens/FilterModal.js
+++ src/screens/FilterModal.js
@@ -6,7 +6,7 @@ import { Actions } from 'react-native-router-flux';
 const { width, height } = Dimensions.get('window');
 
 const CARD_HEIGHT = height / 4;
-const CARD_WIDTH = width - 100;
+const CARD_WIDTH = width - 75;
 
 class FilterModal extends Component {
   constructor(props) {
@@ -15,21 +15,39 @@ class FilterModal extends Component {
 
   state = {
     filter: {
-      price: [2]
+      price: []
+    },
+    priceOne: false,
+    priceTwo: false
+  };
+
+  handleFinishFilter = () => {
+    Actions.pop({ refresh: { filter: this.state.filter } });
+  };
+
+  priceFilterOne = () => {
+    if (this.state.priceOne) {
+      this.state.filter.price.shift();
+      this.setState({
+        priceOne: false
+      });
+    } else {
+      this.setState({
+        filter: {
+          price: [this.state.filter.price.unshift(1)]
+        },
+        priceOne: true
+      });
     }
   };
 
   render() {
-    console.log(this.props);
+    console.log(this.state);
     return (
       <View style={{ flex: 1 }}>
         <View style={styles.container}>
           <View style={styles.onBackContainer}>
-            <TouchableOpacity
-              onPress={() => {
-                Actions.pop({ refresh: { filter: this.state.filter } });
-              }}
-            >
+            <TouchableOpacity onPress={() => this.handleFinishFilter}>
               <Icon
                 type="entypo"
                 name="chevron-thin-down"
@@ -43,8 +61,21 @@ class FilterModal extends Component {
           <Card title="Filter Search" wrapperStyle={{ alignItems: 'center' }}>
             <Text style={{ fontSize: 18 }}>Price</Text>
             <View style={styles.priceButtonContainer}>
-              <Button title={'$'} />
-              <Button title={'$$'} />
+              <View style={styles.priceButtonStyle}>
+                <Button
+                  title="$"
+                  textStyle={{ color: 'black' }}
+                  buttonStyle={styles.offPriceButtonStyle}
+                  onPress={this.priceFilterOne}
+                />
+              </View>
+              <View style={styles.priceButtonStyle}>
+                <Button
+                  title={'$$'}
+                  buttonStyle={styles.offPriceButtonStyle}
+                  textStyle={{ color: 'black' }}
+                />
+              </View>
             </View>
           </Card>
         </View>
@@ -55,7 +86,7 @@ class FilterModal extends Component {
 
 const styles = {
   container: {
-    backgroundColor: '#D3D3D3',
+    backgroundColor: '#ac7339',
     justifyContent: 'center', // Vertical
     alignItems: 'center', // Horizontal
     height: 80,
@@ -68,7 +99,7 @@ const styles = {
     paddingTop: 25
   },
   onBackContainer: {
-    backgroundColor: '#D3D3D3'
+    backgroundColor: '#ac7339'
   },
   priceContainer: {
     paddingTop: 20,
@@ -83,6 +114,15 @@ const styles = {
     paddingTop: 10,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  priceButtonStyle: {
+    flex: 1
+  },
+  offPriceButtonStyle: {
+    backgroundColor: '#D3D3D3'
+  },
+  onPriceButtonStyle: {
+    backgroundColor: 'brown'
   }
 };
 
