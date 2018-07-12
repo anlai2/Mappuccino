5948e10a4ee3442a517f3c9605ca19f3310f4134
Andy <anlaics2@gmail.com>
2018-07-11 21:01:37 -0700

    first welcome screen design

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { LinearGradient } from 'expo';
 import { Actions } from 'react-native-router-flux';
 
+import WelcomeImage from '../../assets/coffeeImage.jpg';
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+          backgroundColor: '#FFD618'
+        }}
+      >
+        <Image
+          source={WelcomeImage}
+          style={{
+            resizeMode: 'cover',
+            width: 400,
+            height: 400,
+            marginBottom: 10
+          }}
+        />
+        <View style={{ padding: 25 }}>
+          <Text
+            style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 25 }}
+          >
+            Find coffee shops near you with a single button!
+          </Text>
+        </View>
         <TouchableOpacity onPress={() => Actions.map()}>
           <LinearGradient
             colors={['#86592d', '#ac7339', '#c68c53']}
@@ -15,7 +39,7 @@ class WelcomeScreen extends Component {
             <Text
               style={{
                 backgroundColor: 'transparent',
-                fontSize: 15,
+                fontSize: 18,
                 color: '#fff'
               }}
             >
