b7c920cf89e2dd6a1ea8d4fc1070ea6535d573a1
Andy <anlaics2@gmail.com>
2018-07-24 18:28:17 -0700

    better card ui for iphone 7, fix in future pls

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -28,7 +28,7 @@ const COFFEE_MARKER = require('../../assets/coffeeIcon.png');
 
 const { width, height } = Dimensions.get('window');
 
-const CARD_HEIGHT = height / 3.9;
+const CARD_HEIGHT = height / 3.5;
 const CARD_WIDTH = width - 75;
 
 export default class TestScreen extends React.Component {
